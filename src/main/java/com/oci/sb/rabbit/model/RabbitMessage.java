770d209fe6a030c408aad1ef0970a9073fbdc1c4
subhashchandrab <subhashchandra.b@gmail.com>
2021-12-17 12:38:20 +0530

    Update RabbitMessage.java

--- src/main/java/com/oci/sb/rabbit/model/RabbitMessage.java
+++ src/main/java/com/oci/sb/rabbit/model/RabbitMessage.java
@@ -3,21 +3,32 @@ package com.oci.sb.rabbit.model;
 public class RabbitMessage {
 	private int id;
 	private String content;
+
+	public RabbitMessage(int id, String content) {
+		super();
+		this.id = id;
+		this.content = content;
+	}
+	
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getContent() {
 		return content;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
+
 	@Override
 	public String toString() {
 		return "RabbitMessage [id=" + id + ", content=" + content + "]";
 	}
-	
+
 }
